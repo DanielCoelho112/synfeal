227573544ac0981aa77f9af6dffaa80b2198bd1a
Daniel Coelho <danielsilveiracoelho@ua.pt>
2022-05-16 12:12:19 +0100

    #52; add bash scripts to combine the poses of the sfm batches

---
+++ bash_scripts/sfm_convert_to_initial_frame.sh
@@ -0,0 +1,34 @@
+rosrun localbot_localization sfm_convert_to_initial_frame  -p_sfm 'seq56_sfm_0_499'
+
+rosrun localbot_localization sfm_convert_to_initial_frame  -p_sfm 'seq56_sfm_499_998'
+
+rosrun localbot_localization sfm_convert_to_initial_frame  -p_sfm 'seq56_sfm_998_1497'
+
+rosrun localbot_localization sfm_convert_to_initial_frame  -p_sfm 'seq56_sfm_1497_1996'
+
+rosrun localbot_localization sfm_convert_to_initial_frame  -p_sfm 'seq56_sfm_1996_2495'
+
+rosrun localbot_localization sfm_convert_to_initial_frame  -p_sfm 'seq56_sfm_2495_2994'
+
+rosrun localbot_localization sfm_convert_to_initial_frame  -p_sfm 'seq56_sfm_2994_3493'
+
+rosrun localbot_localization sfm_convert_to_initial_frame  -p_sfm 'seq56_sfm_3493_3992'
+
+rosrun localbot_localization sfm_convert_to_initial_frame  -p_sfm 'seq56_sfm_3992_4491'
+
+rosrun localbot_localization sfm_convert_to_initial_frame  -p_sfm 'seq56_sfm_4491_4990'
+
+rosrun localbot_localization sfm_convert_to_initial_frame  -p_sfm 'seq56_sfm_4990_5489'
+
+rosrun localbot_localization sfm_convert_to_initial_frame  -p_sfm 'seq56_sfm_5489_5988'
+
+rosrun localbot_localization sfm_convert_to_initial_frame  -p_sfm 'seq56_sfm_5988_6487'
+
+rosrun localbot_localization sfm_convert_to_initial_frame  -p_sfm 'seq56_sfm_6487_6986'
+
+rosrun localbot_localization sfm_convert_to_initial_frame  -p_sfm 'seq56_sfm_6986_7485'
+
+rosrun localbot_localization sfm_convert_to_initial_frame  -p_sfm 'seq56_sfm_7485_7984'
+
+rosrun localbot_localization sfm_convert_to_initial_frame  -p_sfm 'seq56_sfm_7984_8664'
+
---
+++ bash_scripts/sfm_scale_transformations.sh
@@ -0,0 +1,34 @@
+rosrun localbot_localization sfm_scale_transformations  -p_sfm 'seq56_sfm_0_499' -d 'seq56'
+ 
+rosrun localbot_localization sfm_scale_transformations  -p_sfm 'seq56_sfm_499_998' -d 'seq56'
+
+rosrun localbot_localization sfm_scale_transformations  -p_sfm 'seq56_sfm_998_1497' -d 'seq56'
+
+rosrun localbot_localization sfm_scale_transformations  -p_sfm 'seq56_sfm_1497_1996' -d 'seq56'
+
+rosrun localbot_localization sfm_scale_transformations  -p_sfm 'seq56_sfm_1996_2495' -d 'seq56'
+
+rosrun localbot_localization sfm_scale_transformations  -p_sfm 'seq56_sfm_2495_2994' -d 'seq56'
+
+rosrun localbot_localization sfm_scale_transformations  -p_sfm 'seq56_sfm_2994_3493' -d 'seq56'
+
+rosrun localbot_localization sfm_scale_transformations  -p_sfm 'seq56_sfm_3493_3992' -d 'seq56'
+
+rosrun localbot_localization sfm_scale_transformations  -p_sfm 'seq56_sfm_3992_4491' -d 'seq56'
+
+rosrun localbot_localization sfm_scale_transformations  -p_sfm 'seq56_sfm_4491_4990' -d 'seq56'
+
+rosrun localbot_localization sfm_scale_transformations  -p_sfm 'seq56_sfm_4990_5489' -d 'seq56'
+
+rosrun localbot_localization sfm_scale_transformations  -p_sfm 'seq56_sfm_5489_5988' -d 'seq56'
+
+rosrun localbot_localization sfm_scale_transformations  -p_sfm 'seq56_sfm_5988_6487' -d 'seq56'
+
+rosrun localbot_localization sfm_scale_transformations  -p_sfm 'seq56_sfm_6487_6986' -d 'seq56'
+
+rosrun localbot_localization sfm_scale_transformations  -p_sfm 'seq56_sfm_6986_7485' -d 'seq56'
+ 
+rosrun localbot_localization sfm_scale_transformations  -p_sfm 'seq56_sfm_7485_7984' -d 'seq56'
+
+rosrun localbot_localization sfm_scale_transformations  -p_sfm 'seq56_sfm_7984_8664' -d 'seq56'
+
